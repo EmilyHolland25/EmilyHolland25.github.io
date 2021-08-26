7136d6ec250ab701fac175a8e7533e0811fd169b
Emily Holland <emilyholland25@yahoo.com>
2021-08-26 21:03:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -116,27 +116,29 @@ const title = document.querySelector(".title");
 const image = document.querySelector('.openImage');
 const logo = document.querySelector('.logo');
 
-function typing(){
-  const texts = ['Developer', 'Designer'];
-  let count = 0;
-  let index = 0;
-  let currentText = '';
-  let letter = '';
-
-  (function type(){
-    if(count === texts.length){
-      count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
-
-    document.querySelector('.typing').textContent = letter;
-    if(letter.length === currentText.length){
-      count++;
-      index = 0;
-    }
-    setTimeout(type, 180);
-  }());
-}
-
-typing();
+// function typing(){
+//   const texts = ['Developer', 'Designer'];
+//   let count = 0;
+//   let index = 0;
+//   let currentText = '';
+//   let letter = '';
+//   let wait = parseInt(wait, 10);
+//   let isDeleting = false;
+//
+//   (function type(){
+//     if(count === texts.length){
+//       count = 0;
+//     }
+//     currentText = texts[count];
+//     letter = currentText.slice(0, ++index);
+//
+//     document.querySelector('.typing').textContent = letter;
+//     if(letter.length === currentText.length){
+//       count++;
+//       index = 0;
+//     }
+//     setTimeout(type, 120);
+//   }());
+// }
+//
+// typing();
